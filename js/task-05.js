d52cf08903bd954b2d6318ffea644f7b3db57f80
Eugene Plotnytskyi <eugeneplotnytskyi@gmail.com>
2022-09-15 19:28:43 +0300

    fifth done final

--- js/task-05.js
+++ js/task-05.js
@@ -10,7 +10,3 @@ const addtoSpanEl = inputEl.addEventListener("input", (event) => {
 
 
 
-// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка "Anonymous".
-
-// <input type="text" id="name-input" placeholder="Please enter your name" />
-// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
