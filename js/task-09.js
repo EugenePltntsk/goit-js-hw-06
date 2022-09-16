b8ab6146bef86845a3bdf055c83bb3ab0432a0ec
Eugene Plotnytskyi <eugeneplotnytskyi@gmail.com>
2022-09-16 11:22:22 +0300

    ninth done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.body;
+const buttonEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+buttonEl.addEventListener("click", () => {
+  const goalColor = getRandomHexColor();
+  spanEl.textContent = goalColor;
+  bodyEl.style.backgroundColor = goalColor;
+});
+
+
+
