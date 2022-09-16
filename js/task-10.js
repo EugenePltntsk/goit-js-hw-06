1585a610dc0f9250c20c039a86e54b9dc8958bf1
Eugene Plotnytskyi <eugeneplotnytskyi@gmail.com>
2022-09-16 12:51:57 +0300

    tenth done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputEl = document.querySelector("[type = 'number']");
+const buttonCr = document.querySelector("[data-create]");
+const buttonDes = document.querySelector("[data-destroy]");
+const mainEl = document.querySelector("#boxes")
+
+ 
+const createBoxes = () => { 
+  const numberOfBoxes = inputEl.value;
+  const arrElements = [];
+  const defaultSize = 30;
+  for (let i = 0; i < numberOfBoxes; i += 1) {
+    const currentSize = defaultSize + i * 10;
+    const newBox = document.createElement("div");
+    newBox.style.width = currentSize + "px";
+    newBox.style.height = currentSize + "px";
+    newBox.style.backgroundColor = getRandomHexColor();
+    arrElements.push(newBox);
+  }
+  mainEl.append(...arrElements);
+}
+ 
+buttonCr.addEventListener("click", createBoxes);
+
+const destroyBoxes = () => { 
+  mainEl.innerHTML = "";
+}
+
+buttonDes.addEventListener("click", destroyBoxes);
