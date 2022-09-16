575a6d6aa0c73fa35d83b25aed47a6e965a10484
Eugene Plotnytskyi <eugeneplotnytskyi@gmail.com>
2022-09-16 10:17:23 +0300

    sixth done final

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,17 @@
+
 const inputEl = document.querySelector("#validation-input");
+const goalLength = inputEl.getAttribute("data-length");
 
-inputEl.addEventListener("blur", (event) => {
-  if (event.target.value.length == inputEl.getAttribute("data-length")) {
+inputEl.addEventListener("blur", event => {
+  
+  if (event.target.value.length === Number(goalLength)) {
     inputEl.classList.add("valid");
-    if (inputEl.classList.contains("invalid")) {
-      inputEl.classList.remove("invalid");
-    }
-  } else {
-    if (inputEl.classList.contains("valid")) {
-      inputEl.classList.remove("valid");
-    }
-    inputEl.classList.add("invalid");
-  }
-});
+    inputEl.classList.remove("invalid");
 
+    ;
+  } else {
+    inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
+  }
+
+});
